70c9eb10e99cec00401d35965b8b5d274bb185b1
Matthew Restine <Digitalopensky@gmail.com>
2017-06-28 12:56:36 -0500

    section 2 completed

--- React_Redux_Sandbox/ReduxSimpleStarter/src/index.js
+++ React_Redux_Sandbox/ReduxSimpleStarter/src/index.js
@@ -1,23 +1,36 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
 
 const API_KEY = 'AIzaSyAVsTJ53aXsCuHa2UivA5pQamxnQmaPMFU';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
-        YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
-            this.setState({ videos });
+        this.state = { 
+            videos: [],
+            selectedVideo: null 
+        };
+        this.videoSearch('SWA Landing');
+    }
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term:term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
         });
     }
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 1000);
         return (<div>
-            <SearchBar />
-            <VideoList videos={this.state.videos} />
+            <SearchBar onSearchTermChange={videoSearch}/>
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
         </div>);
     }
 }
